fe5808c7a55275b3827c568675ef9cdd82ecd6b2
Vipul Avinash <vipulavinash123@gmail.com>
2026-07-25 15:03:16 +0530

    Database and Auth Implementation

---
+++ src/app/(auth)/sign-in.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native"
+
+
+ const SignInScreen =()=>{
+
+    return(
+        <View>
+            <Text>Sign In Screen</Text>
+        </View>
+    )
+
+}
+
+export default SignInScreen
